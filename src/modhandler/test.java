93c066ecda8a99967a3b70a31e1f080b6da2ce1e
Nico <elmuches@googlemail.com>
2016-07-03 15:35:16 +0200

    Revert "test.java: int i zum zweiten"
    This reverts commit aaa45aa6ca4d3a5f7773ab380293d76bb5c4d570.

---
+++ src/modhandler/test.java
@@ -0,0 +1,20 @@
+package modhandler;
+
+/**
+ * Created by Nico on 03.07.2016.
+ */
+public class test
+{
+
+    int i;
+
+    public test(int i) {
+        this.i = i;
+    }
+
+    public test()
+	{
+		System.out.println("Döner macht schöner...");
+	}
+
+}
