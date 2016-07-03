4c2a2e288a1d0bf32dab169caf39aa2cd06fc7cc
Krassus <krassus@krassus.net>
2016-07-03 13:53:03 +0200

    Grundgerüst

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,11 +3,13 @@ package main;
 import gui.Gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import modhandler.ModHandler;
 
 public class Main extends Application
 {
 	public static final Configuration config = new Configuration();
 	public static final Language lang = new Language(Main.config.langFile);
+	public static final ModHandler modHandler = new ModHandler();
 	
 	public static void main(String[] args)
 	{
